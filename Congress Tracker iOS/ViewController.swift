6d06e5a47b8adfbb750035779afdda654c2b86b0
Jeffrey Knight <Jeffrey.757Knight@gmail.com>
2020-03-24 13:55:07 -0400

    Initial Commit

---
+++ Congress Tracker iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Congress Tracker iOS
+//
+//  Created by jeffrey knight on 3/24/20.
+//  Copyright © 2020 fullsailStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
